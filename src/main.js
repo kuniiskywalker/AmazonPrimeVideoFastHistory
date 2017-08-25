929b6fe6a5ed69bb5b5799aa99d89167fec0e0e5
kuniiskywalker <kuniiskywalker@gmail.com>
2017-08-25 18:30:56 +0900

    clean

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-var language = document.querySelector('.nav-a.nav-a-2.icp-link-style-2').text.trim();
+const language = document.querySelector('.nav-a.nav-a-2.icp-link-style-2').text.trim();
 
-const link = document.createElement('a'); 
+const viewing_history_url = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF8&collection=watched';
+
 const label = language != "JP" ? "Viewing History": chrome.i18n.getMessage("menuLabel");
 
+const link = document.createElement('a'); 
 link.innerHTML = `<span class="nav-a-content">${label}</span>`;
 link.className = 'nav-a nav-right';
-link.href = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF8&collection=watched';
+link.href = viewing_history_url;
 
 const nav = document.querySelector('#nav-subnav[data-category="instant-video"]');
 nav.insertBefore(link, nav.querySelectorAll('a')[9]);
