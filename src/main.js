c7be4816d4a13f231a76f280d721efbd0226cd30
kuniiskywalker <kuniiskywalker@gmail.com>
2017-08-25 16:41:02 +0900

    英語版対応

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+var language = document.querySelector('.nav-a.nav-a-2.icp-link-style-2').text.trim();
+
 const link = document.createElement('a'); 
-link.innerHTML = '<span class="nav-a-content">視聴履歴</span>';
+const label = language != "JP" ? "Viewing History": chrome.i18n.getMessage("menuLabel");
+
+link.innerHTML = `<span class="nav-a-content">${label}</span>`;
 link.className = 'nav-a nav-right';
 link.href = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF8&collection=watched';
 
 const nav = document.querySelector('#nav-subnav[data-category="instant-video"]');
-nav.insertBefore(element, nav.querySelectorAll('a')[9]);
+nav.insertBefore(link, nav.querySelectorAll('a')[9]);
