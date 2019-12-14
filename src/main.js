eaaf6a6045264922843b992d389cb72a7f6ae4a1
國井弘一 <kuniiskywalker@gmail.com>
2019-12-14 21:22:49 +0900

    Bug fix that menu is not displayed in non-login state

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
-const language = document.querySelector('.nav-a.nav-a-2.icp-link-style-2').text.trim();
+const viewingHistoryUrl = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF8&collection=watched';
 
-const viewing_history_url = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF8&collection=watched';
+const languageCode = document.querySelector('.nav-a.nav-a-2.icp-link-style-2').text.trim();
 
-const label = language != "JP" ? "Viewing History": chrome.i18n.getMessage("menuLabel");
+const viewingHistoryLabel = (() => {
+    switch (languageCode) {
+        case "JP": return "視聴履歴";
+        case "ZH": return "查看历史";
+        default : return "Viewing History";
+    }
+})();
 
-// 新しいノードの挿入先の親要素への参照を取得
-const parentElement = document.querySelector('.av-retail-m-nav-list-subitems');
+if (document.querySelector('.DVWebNode-retail-nav-wrapper')) {
+    addLoginMenu(viewingHistoryUrl, viewingHistoryLabel);
+} else {
+    addNoLoginMenu(viewingHistoryUrl, viewingHistoryLabel);
+}
 
-// 子要素への参照を取得
-const theFirstChild = parentElement.firstChild;
+function addLoginMenu(viewingHistoryUrl, label)
+{
+    const parentElement = document.querySelector('.av-retail-m-nav-list-subitems');
 
-// 新規の要素の作成
-const newElement = document.createElement('li');
-newElement.innerHTML = `<a href="${viewing_history_url}">${label}</a>`;
-newElement.className = 'av-retail-m-nav-subitem';
+    const theFirstChild = parentElement.firstChild;
 
-// 最初の子要素の前に新規の要素を挿入
-parentElement.insertBefore(newElement, theFirstChild);
+    const link = document.createElement('li');
+    link.innerHTML = `<a href="${viewingHistoryUrl}">${label}</a>`;
+    link.className = 'av-retail-m-nav-subitem';
+
+    parentElement.insertBefore(link, theFirstChild);
+}
+
+function addNoLoginMenu(viewingHistoryUrl, label)
+{
+    const parentElement = document.querySelector('#nav-subnav');
+
+    const link = document.createElement('a');
+    link.innerHTML = `<span class="nav-a-content">${label}</span>`;
+    link.className = 'nav-a nav-right';
+    link.href = viewingHistoryUrl;
+
+    parentElement.appendChild(link);
+}
