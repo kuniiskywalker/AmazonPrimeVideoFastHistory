23ce0770992c98ab5000c60693282a1ca77d284f
國井弘一 <kuniiskywalker@gmail.com>
2019-12-14 18:46:32 +0900

    New UI support

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ const viewing_history_url = '/gp/yourstore/iyr/ref=pd_ys_iyr_edit_watched?ie=UTF
 
 const label = language != "JP" ? "Viewing History": chrome.i18n.getMessage("menuLabel");
 
-const link = document.createElement('a'); 
-link.innerHTML = `<span class="nav-a-content">${label}</span>`;
-link.className = 'nav-a nav-right';
-link.href = viewing_history_url;
+// 新しいノードの挿入先の親要素への参照を取得
+const parentElement = document.querySelector('.av-retail-m-nav-list-subitems');
 
-const nav = document.querySelector('#nav-subnav[data-category="instant-video"]');
-nav.insertBefore(link, nav.querySelectorAll('a')[9]);
+// 子要素への参照を取得
+const theFirstChild = parentElement.firstChild;
+
+// 新規の要素の作成
+const newElement = document.createElement('li');
+newElement.innerHTML = `<a href="${viewing_history_url}">${label}</a>`;
+newElement.className = 'av-retail-m-nav-subitem';
+
+// 最初の子要素の前に新規の要素を挿入
+parentElement.insertBefore(newElement, theFirstChild);
